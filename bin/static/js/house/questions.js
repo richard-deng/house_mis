81ea08a8da77027fb846f1e0c3d5e5b3a664300c
邓成 <dengcheng@qfpay.com>
2018-08-04 15:43:55 +0800

    add

--- bin/static/js/house/questions.js
+++ bin/static/js/house/questions.js
@@ -78,6 +78,9 @@ $(document).ready(function () {
                         $("#normal_question_category").text(obj.original.category);
                         $('#addQuestionCreateForm').resetForm();
                         $("label.error").remove();
+                        // 添加时默认的是普通文本
+                        $('#rich_text_add_div').hide();
+
                         $("#addQuestionModal").modal();
                         /*
                         inst.create_node(obj, {}, "last", function (new_node) {
@@ -579,6 +582,65 @@ $(document).ready(function () {
         var sel_id = sel[0];
         create_node(sel_id, description, 3);
         $('#addDescModal').modal('hide');
+    });
+
+    $('#save_type_add').change(function () {
+        var save_type = $('#save_type_add').val();
+        if(save_type === "1"){
+            // 1是普通文本
+            $('#rich_text_add_div').hide();
+        } else {
+            // 2是富文本
+            $('#save_type_add').hide();
+        }
+    });
+
+    $('#summernote').summernote({
+        minHeight: 420,
+        // maxHeight: 320,
+        minWidth: 512,
+        // maxWidth: 512,
+        focus: true,
+        lang: 'zh-CN',
+        dialogsInBody: true,
+        toolbar: [
+            // [groupName, [list of button]]
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['insert', ['picture', 'link']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+        ],
+        callbacks: {
+            onImageUpload: function(files) {
+                //由于summernote上传图片上传的是二进制数据
+                //所以这里可以自己重新上传图片方法
+                var formData = new FormData();
+                var name = files[0]['name'];
+                console.log('name:', name);
+                formData.append('file',files[0]);
+                formData.append("name", name);
+                $.ajax({
+                    url : '/mis/v1/api/icon/upload', //后台文件上传接口
+                    type : 'POST',
+                    data : formData,
+                    processData : false,
+                    contentType : false,
+                    success : function(data) {
+                        console.log('data:', data);
+                        detail_data = data.data;
+                        src = detail_data.icon_url;
+                        //设置到编辑器中
+                        $('#summernote').summernote('insertImage',src,'img');
+                    },
+                    error:function(){
+                        alert("上传失败...");
+                    }
+                });
+            }
+        }
     });
 
     function create_node(sel_id, name, category) {
